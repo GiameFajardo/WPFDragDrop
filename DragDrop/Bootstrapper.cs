b103dae841e1477a5ed3464ab34c35df32a2ae16
Giame Fajardo <gmcrls13@gmail.com>
2019-02-23 21:15:26 -0400

    first commit

---
+++ DragDrop/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using DragDrop.Views;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DragDrop
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<MainWindow>();
+        }
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ DragDrop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragDrop.ViewModels
+{
+    public class MainWindowViewModel: BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            List1 = new ObservableCollection<Person>();
+            List1.Add(new Person { Name = "Giame" });
+        }
+        public ObservableCollection<Person> _list1;
+        public ObservableCollection<Person> List1 { get { return _list1; }
+            set
+            {
+                _list1 = value;
+            }
+        }
+    }
+
+    public class Person
+    {
+
+        public string Name { get; set; }
+    }
+}
